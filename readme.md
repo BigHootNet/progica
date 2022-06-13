827d931db07f6a84adb1f01f6e3a3798446bd2de
BigHootNet <thetrigun@gmail.com>
2022-06-13 02:29:29 +0200

    Version 0.002
    0.002 Upgrade

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Version 0.002
+
+### Major Update
+
+ - User system added
+ - Role system added
+ - Login / Logout system added
+ - Admin menu hidden for random users
+
+### Troubles encountered :
+
+- none
--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -25,7 +25,7 @@ class AdminController extends AbstractController
         ]);
     }
 
-        /**
+    /**
      * @route("/admin/gite/create", name="admin_gite_create")
      */
     public function create(ManagerRegistry $doctrine, Request $request)
@@ -39,11 +39,54 @@ class AdminController extends AbstractController
             $em = $doctrine->getManager();
             $em->persist($gite);
             $em->flush();
+            $this->addFlash('success', 'Votre gite a bien été créé');
             return $this->redirectToRoute('admin_index');
         }
 
         return $this->render('admin/gite/create.html.twig', [
             "formGite" => $form->createView()
         ]);
+    }
+
+    /**
+     * @route("/admin/gite/edit/{id}", name="admin_gite_edit")
+     */
+    public function edit(Gite $gite, Request $request, ManagerRegistry $doctrine)
+    {
+        $form = $this->createForm(GiteType::class, $gite);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em = $doctrine->getManager();
+            $em->flush();
+            $this->addFlash('success', 'Votre gite a bien été edité');
+            return $this->redirectToRoute('admin_index');
+        }
+
+        return $this->render('admin/gite/edit.html.twig', [
+        "formGite" => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/gite/delete/{id}", name="admin_gite_delete")
+     */
+    public function delete(Gite $gite, ManagerRegistry $doctrine, Request $request)
+    {
+        if($this->isCsrfTokenValid("gite_delete". $gite->getId(), $request->request->get('token'))) 
+        {
+            $em = $doctrine->getManager();
+            $em->remove($gite);
+            $em->flush();
+            $this->addFlash('success', 'Le gite a bien été supprimé');
+        }
+        else
+        {
+            $this->addFlash('error', 'Token non valide');
+        }
+        return $this->redirectToRoute('admin_index');
+        
     }
 }
